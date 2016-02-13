dbdef6fd0d92de163dc545ebd347632b3191c299
Fortyseven <fortyseven@gmail.com>
2016-02-13 08:06:13 -0500

    Initial commit

---
+++ app/popup.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var Popup = function()
+{
+    var _self = this;
+
+
+
+// /**************************************************************************/
+// var popup = null;
+
+// window.onload = function()
+// {
+//     popup = new Popup();
+// };
---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var minify_html = require('gulp-minify-html');
+var minify_css = require('gulp-minify-css');
+var del = require('del');
+var es = require('event-stream');
+var run_sequence = require('run-sequence');
+var zip = require('gulp-zip');
+var debug  = require('gulp-debug');
+
+//var concat = require('gulp-concat');
+
+/***********************/
+var OUTPUT_PATH = 'dist/';
+
+/***********************/
+gulp.task('misc', function(){
+	return gulp.src( ['manifest.json', 'LICENSE'] )			
+                   .pipe(gulp.dest(OUTPUT_PATH));	
+});
+
+/***********************/
+gulp.task('vendor', function() {
+	return gulp.src(
+		['vendor/google/material-design-lite.js'])
+		.pipe(uglify()).on('error', errorHandler)
+		.pipe(gulp.dest(OUTPUT_PATH + "vendor/google/"));
+});
+
+/***********************/
+gulp.task('bower', function(){
+	return gulp.src(
+		['bower_components/micromustache/dist/**/*.min.js',
+		 'bower_components/zepto/zepto.min.js',
+         'bower_components/sweetalert/dist/sweetalert.min.js'        
+         ]
+		, {base: './bower_components'})
+		.pipe(uglify())
+		.pipe(gulp.dest(OUTPUT_PATH + "bower_components"));
+});
+
+/***********************/
+gulp.task('css', function(){
+	var css_from_less = gulp.src(
+        ['app/assets/*.less', 
+         'bower_components/sweetalert/dist/sweetalert.css'
+         ])
+		.pipe(less());
+							
+	var css = gulp.src('app/assets/*.css');
+	
+	return es.merge(css_from_less, css)
+		.pipe(minify_css())
+		.pipe(gulp.dest(OUTPUT_PATH + 'app/assets/'));
+	
+});
+
+/***********************/
+gulp.task('images', function(){
+	return gulp.src('app/assets/*.png')
+		.pipe(gulp.dest(OUTPUT_PATH+"app/assets/"));
+});
+
+/***********************/
+// gulp.task('scripts', /*['dep'],*/function() {
+// 	return gulp.src('app/**/*.js')
+// 	//.pipe(concat())
+// 	.pipe(uglify()).on('error', errorHandler)
+// 	.pipe(gulp.dest(OUTPUT_PATH + 'app'));    
+// });
+
+/***********************/
+gulp.task('html', /*['dep'],*/function() {
+	return gulp.src('app/**/*.html', {base: './app'})	
+	.pipe(minify_html()).on('error', errorHandler)
+	.pipe(gulp.dest(OUTPUT_PATH + 'app'));    
+});
+
+/***********************/
+gulp.task('scripts', /*['dep'],*/function() {
+	return gulp.src('app/**/*.js')
+	//.pipe(concat())
+	//.pipe(uglify()).on('error', errorHandler)
+	.pipe(gulp.dest(OUTPUT_PATH + 'app'));    
+});
+
+/***********************/
+gulp.task('pack', function() {
+	console.log("Creating BUILD.ZIP...");
+	console.log("https://chrome.google.com/webstore/developer/dashboard");
+	return gulp.src(OUTPUT_PATH + "**/*")
+		.pipe(zip('build.zip'))
+		.pipe(gulp.dest('.'));
+});
+
+/***********************/
+gulp.task('clean', function(callback) {
+	del(['build.zip']);
+	return del(['dist'], callback);
+});
+
+/***********************/
+gulp.task('build', ['misc', 'css', 'images', 'vendor', 'bower', 'html', 'scripts']);
+
+/***********************/
+gulp.task('default', function(callback){
+	return run_sequence('clean', ['build'], 'pack', callback); 	
+});
+
+function errorHandler(error) {
+	console.log(error.toString());
+	this.emit('end');
+}
